3b82a7f8f4f43c7977ee871323fe7886f29f0719
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-03-10 15:44:02 -0300

    docs: updates dependencies and removes comments

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['res.cloudinary.com'], // Adicione aqui o domínio
+    domains: ['res.cloudinary.com'], 
 
     remotePatterns: [
       
       {
-        protocol: 'http', // Para imagens de Cloudinary
+        protocol: 'http', 
         hostname: 'res.cloudinary.com', // Domínio de Cloudinary
       },
     ],
