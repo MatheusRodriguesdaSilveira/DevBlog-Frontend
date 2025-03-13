f066e0e595cd6ef050323d49f9e99937584aa924
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-03-13 14:03:26 -0300

    style: update format

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['res.cloudinary.com'], 
+    domains: ["res.cloudinary.com"],
 
     remotePatterns: [
-      
       {
-        protocol: 'http', 
-        hostname: 'res.cloudinary.com', // Domínio de Cloudinary
+        protocol: "http",
+        hostname: "res.cloudinary.com", // Domínio de Cloudinary
       },
     ],
   },
