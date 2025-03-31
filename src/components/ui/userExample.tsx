7f87b17d8505c57d905fde59af2345024332ed01
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-03-31 14:37:20 -0300

    add: new request to capture unfolloweds users

--- src/components/ui/userExample.tsx
+++ src/components/ui/userExample.tsx
@@ -14,10 +14,9 @@ import {
   DialogTrigger,
 } from "./dialog";
 
-import { Button } from "./button";
-import { Label } from "./label";
 import { DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
-import { UserRoundSearch } from "lucide-react";
+import { UserPlus, UserRoundSearch } from "lucide-react";
+import { jwtDecode } from "jwt-decode";
 
 interface UserData {
   user: string;
@@ -36,6 +35,8 @@ interface UserData {
   content: string;
   likes: string;
   initialLiked: boolean;
+  followers: string;
+  following: string;
 }
 
 export const UserTemplate = () => {
@@ -43,7 +44,13 @@ export const UserTemplate = () => {
   const [selectedUserId, setSelectedUserId] = useState<string>("");
   const [posts, setPosts] = useState<UserData[]>([]);
 
-  const handleSelectUserPost = (userPostInfo: { userId: string }) => {
+  const [following, setFollowing] = useState(false);
+  const [followers, setFollowers] = useState(false);
+  const [usersFollowed, setUsersFollowed] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  const userSelect = (userPostInfo: { userId: string }) => {
     const { userId } = userPostInfo;
     console.log(`id user: ${userId}`);
     setSelectedUserId(userId);
@@ -55,28 +62,21 @@ export const UserTemplate = () => {
         const token = getCookie("login");
 
         if (!token) {
-          throw new Error("Token not found");
+          console.error("Error loading user data: Token not found");
+          return;
         }
 
-        const response = await api.get("/profiles", {
+        const decoded = jwtDecode(token as string) as { sub: string };
+
+        const currentUserId = decoded.sub;
+
+        const response = await api.get(`/following`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
 
-        const post = response.data.posts?.map(
-          (post: {
-            id: string;
-            imageUrl: string;
-            title: string;
-            description: string;
-          }) => {
-            return post;
-          }
-        );
-
-        setPosts(post || []);
-        setUsers(response.data || []);
+        setUsers(response.data);
       } catch (error) {
         console.error("Error loading user data:", error);
       }
@@ -109,14 +109,63 @@ export const UserTemplate = () => {
         }
       );
 
+      const follower = response.data.followers?.map(
+        (follower: { id: string; followerId: string }) => {
+          return follower;
+        }
+      );
+
+      const following = response.data.following?.map(
+        (following: { id: string; followedId: string }) => {
+          return following;
+        }
+      );
+
+      console.log("user selecionado", userId);
+      console.log("following", following);
+      console.log("follower", follower);
+
+      setFollowing(following?.length);
+      setFollowers(follower?.length);
       setPosts(post || []);
     } catch (error) {
       console.error("Error loading user data:", error);
     }
   };
 
+  const handleFollowButton = async (userId: string) => {
+    try {
+      const token = getCookie("login");
+      if (!token) {
+        console.error("Cookie not found");
+        return;
+      }
+      const decoded = jwtDecode(token as string) as { sub: string };
+
+      const currentUserId = decoded.sub;
+      const formData = new FormData();
+      formData.append("followedId", currentUserId);
+      formData.append("followerId", userId);
+
+      const response = await api.post("/follow", formData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      console.log(`${currentUserId} following ${selectedUserId}`);
+
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error loading user data:", error);
+    } finally {
+      setLoading(false);
+      window.location.reload();
+    }
+  };
+
   return (
-    <div className="xl:max-h-[250px] 2xl:max-h-[300px] overflow-y-auto mx-1 2xl:mx-0">
+    <div className="md:max-h-[250px] xl:max-h-[250px] 2xl:max-h-[300px] overflow-y-auto mx-1 2xl:mx-0">
       {users.map((user, index) => (
         <div
           key={index}
@@ -141,12 +190,14 @@ export const UserTemplate = () => {
               </div>
             </DialogTrigger>
 
-            <DialogContent className="sm:max-w-[1000px] bg-zinc-200 dark:bg-zinc-900">
+            <DialogContent className="md:max-w-[600px] lg:max-w-[1000px] bg-zinc-200 dark:bg-zinc-900">
               <DialogHeader>
-                <span className="text-zinc-700 dark:text-zinc-200 flex gap-1 items-center">
+                <DialogTitle className="text-zinc-700 dark:text-zinc-200 flex gap-1 items-center">
                   Preview
                   <UserRoundSearch className="size-4" />
-                </span>
+                </DialogTitle>
+                <DialogDescription></DialogDescription>
+
                 <div className="flex items-center gap-3">
                   <Image
                     src={user.profilePicture || User}
@@ -154,40 +205,56 @@ export const UserTemplate = () => {
                     width={100}
                     height={100}
                     priority
-                    className="2xl:w-24 2xl:h-24 xl:w-20 xl:h-20 rounded-full border border-red-700 flex-shrink-0"
+                    className="2xl:w-24 2xl:h-24 xl:w-20 xl:h-20 md:w-16 md:h-16 rounded-full border border-red-700 flex-shrink-0"
                   />
                   <div>
-                    <DialogTitle>{user.name}</DialogTitle>
-                    <Label className="text-zinc-700 dark:text-zinc-200">
+                    <div className="font-bold text-zinc-700 dark:text-zinc-200 flex gap-2 items-baseline">
+                      {user.name} -
+                      <button
+                        onClick={() => {
+                          handleFollowButton(user.id);
+                        }}
+                        className="flex rounded-md px-2 py-1 bg-red-600 hover:bg-red-900 duration-300"
+                      >
+                        <div className="text-xs flex gap-1">
+                          <UserPlus className="size-5" />
+                        </div>
+                      </button>
+                    </div>
+                    <div className="text-sm text-zinc-700 dark:text-zinc-200">
                       {user.descriptionProfile
                         ?.split("\n")
                         .map((item, index) => (
-                          <DialogDescription key={index}>
+                          <div
+                            key={index}
+                            className="mb-5 text-xs lg:text-sm text-zinc-700 dark:text-zinc-200"
+                          >
                             {item}
-                          </DialogDescription>
-                        )) || "Sem descrição"}
-                    </Label>
-                    <div
-                      key={user.id}
-                      className="flex gap-5 font-semibold mt-5"
-                    >
-                      <p className="text-zinc-700 dark:text-zinc-200 leading-[10px]">
-                        {posts?.length || 0} publicações
-                      </p>
-                      <p className="text-zinc-700 dark:text-zinc-200 leading-[10px]">
-                        {0} seguidores
-                      </p>
-                      <p className="text-zinc-700 dark:text-zinc-200 leading-[10px]">
-                        {0} seguindo
-                      </p>
+                          </div>
+                        )) || "Sem descrição"}
                     </div>
                   </div>
                 </div>
+                <div
+                  key={user.id}
+                  className="flex gap-5 font-semibold items-center justify-center"
+                >
+                  <p className="text-zinc-700 dark:text-zinc-200 leading-[10px]">
+                    {posts?.length || 0} publicações
+                  </p>
+                  <p className="text-zinc-700 dark:text-zinc-200 leading-[10px]">
+                    {followers} seguidores
+                  </p>
+                  <p className="text-zinc-700 dark:text-zinc-200 leading-[10px]">
+                    {following} seguindo
+                  </p>
+                </div>
               </DialogHeader>
+
               <div className="flex justify-center items-center">
                 <DropdownMenuSeparator className="w-full" />
               </div>
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-3">
                 {posts.map((post, index) => (
                   <div
                     key={index}
@@ -198,7 +265,7 @@ export const UserTemplate = () => {
                       alt="Post"
                       width={500}
                       height={500}
-                      className="h-[200px] object-contain"
+                      className="object-contain"
                       priority
                     />
                   </div>
@@ -206,8 +273,16 @@ export const UserTemplate = () => {
               </div>
             </DialogContent>
 
-            <button className="flex rounded-md px-2 py-1 bg-red-600 hover:bg-red-900 duration-300">
-              <p className="text-xs">Seguir</p>
+            <button
+              onClick={() => {
+                handleFollowButton(user.id);
+              }}
+              className={`flex rounded-md px-2 py-1 ${
+                loading ? "bg-gray-500" : "bg-red-600"
+              } hover:${loading ? "" : "bg-red-900"} duration-300`}
+              disabled={loading}
+            >
+              <p className="text-xs">{loading ? "Carregando..." : "Seguir"}</p>
             </button>
           </Dialog>
         </div>
